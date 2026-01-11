af07dae2bd15a4679459ef883ee9960a76497fc5
MarcusPauloF <marcus.custodiofernandes@gmail.com>
2026-01-11 19:44:52 -0300

    fix: ajustado espaco nas credenciais

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -1,7 +1,7 @@
 import '../models/user_model.dart';
 
 class AuthRemoteDatasource {
-  static const String _fixedEmail = '	teste@empiricus.com.br';
+  static const String _fixedEmail = 'teste@empiricus.com.br';
   static const String _fixedPassword = '123456';
   static const String _fixedToken = 'token_abc123xyz';
 
