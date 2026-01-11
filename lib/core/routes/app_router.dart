ee661174f22491612503d331e3c05f0ade35a105
MarcusPauloF <marcus.custodiofernandes@gmail.com>
2026-01-11 18:54:07 -0300

    feat: adicionado gerenciador de rotas.

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+
+    return MaterialPageRoute(
+      builder: (_) => const Scaffold(
+        body: Center(
+          child: Text('Rota não encontrada'),
+        ),
+      ),
+    );
+  }
+}
